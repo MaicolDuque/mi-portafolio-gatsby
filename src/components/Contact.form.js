17c3f36f759cd31172033c0633c893673c6892f4
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2020-03-28 17:24:45 -0500

    Get repositories of GitHub

--- src/components/Contact.form.js
+++ src/components/Contact.form.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => (
   <form className="mt-16 text-center">
-    <label for="contact-content" className="block text-gray-700 text-sm font-bold mb-2">
+    <label htmlFor="contact-content" className="block text-gray-700 text-sm font-bold mb-2">
       Cuéntame de esa idea que quieres hacer realidad.
     </label>
     <div className="flex shadow rounded bg-whiteb border p-2">
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,22 +1,50 @@
-import React, {useEffect, useState} from 'react'
-import repos from '../data/repos'
+import React, { useEffect, useState } from 'react'
+// import repos from '../data/repos'
 import Repo from '../components/Repo'
 
-export default () => (
-  
-  
-  
-  <div className="max-w-4xl mx-auto">
-    <header className="text-center">
-      <h2 className="text-3xl font-bold">Mi trabajo en Open Source</h2>
-      <p>Colaboración y contribución de código</p>
-    </header>
-    <ul className="repos-list">
-      {
-        repos.map( (repo, index ) => {
-        return <Repo key={index} repo={repo} />
-        })
-      }
-    </ul>
-  </div>
-);
+export default () => {
+
+  const [repos, setRepos] = useState([]);
+  const [reposCount, setReposCount] = useState([]);
+
+  useEffect(() => {
+
+    const data = sessionStorage.getItem("repos");
+    let myRepos;
+    if(data){
+      myRepos = JSON.parse(data);
+      setReposCount(myRepos.length)
+      myRepos   = myRepos.slice(1,16);
+      return setRepos(myRepos);
+    }
+    async function fetchRepos(){
+      const response  = await fetch("https://api.github.com/users/MaicolDuque/repos");
+      myRepos   = await response.json();        
+      setReposCount(myRepos.length)
+      sessionStorage.setItem("repos", JSON.stringify(myRepos))    
+      setRepos(myRepos);
+    }
+    fetchRepos();
+  }, []);
+
+  return (
+    <div className="max-w-4xl mx-auto mt-12">
+      <header className="text-center">
+        <h2 className="text-3xl font-bold truncate">Mi trabajo en Open Source</h2>
+        <p>Colaboración y contribución de código</p>
+      </header>
+      <ul className="repos-list">
+        {
+          repos.map((repo, index) => {
+            return <Repo key={index} repo={repo} />
+          })
+        }
+      </ul>
+      <div className="mt-8 text-center">
+        <a href="https://github.com/MaicolDuque" className="btn" target="_blank" rel="noopener noreferrer">
+          Ver más en GitHub - {reposCount}
+        </a>
+      </div>
+    </div>
+  )
+};
